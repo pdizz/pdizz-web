1bcfba520202650e0f15e17b1cb9e4ef1e5af1e5
pdizz <pete.albrecht@gmail.com>
2015-06-16 21:30:01 -0600

    blog template

--- public/app/components/blog/blogController.js
+++ public/app/components/blog/blogController.js
@@ -2,8 +2,18 @@
 
 var pdizzApp = angular.module('pdizzApp', []);
 
-pdizzApp.controller('BlogListController', function ($scope, $http) {
-    $http.get('/api/blog/post').success(function (data) {
-        $scope.posts = data._embedded.post;
-    });
-});
+pdizzApp.controller('BlogListController', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('/api/blog/post').success(function (data) {
+            $scope.posts = data._embedded.post;
+        });
+
+        /**
+         * Turn the string into a Date object
+         * @param date
+         * @returns {Date}
+         */
+        $scope.toDate = function(date) {
+            return new Date(Date.parse(date));
+        }
+    }]);
