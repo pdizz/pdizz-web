f91fe74a64c4795871e0fd24544c30d63a5ff28a
pdizz <pete.albrecht@gmail.com>
2015-07-30 23:52:29 -0600

    removed rootScope from auth module

--- public/app/auth/AuthModule.js
+++ public/app/auth/AuthModule.js
@@ -2,10 +2,9 @@
 
 var AuthModule = angular.module('AuthModule', []);
 
-AuthModule.controller('LoginController', ['$scope', '$rootScope', '$cookieStore', '$state',
-    function ($scope, $rootScope, $cookieStore, $state) {
+AuthModule.controller('LoginController', ['$scope', '$cookieStore', '$state',
+    function ($scope, $cookieStore, $state) {
         $scope.login = function () {
-            $rootScope.token = $scope.token;
             $cookieStore.put('token', $scope.token);
             $state.go('blog');
         };
